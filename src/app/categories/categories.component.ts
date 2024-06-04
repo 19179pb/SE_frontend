ab965af8eec262aeea9806765c38fac33298bdcc
19179pb <19179@student.pb.edu.pl>
2024-06-04 18:12:29 +0200

    dodano login component i dialog dla kategorii

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTable, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,8 +6,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Category } from '../models/category';
 import { CategoryService } from '../services/category.service';
+import { FormsModule, ReactiveFormsModule, FormGroup, NonNullableFormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
@@ -52,4 +54,51 @@ export class CategoriesComponent {
       this.categories.push(result);
       this.table.renderRows();
     });
+  }
+}
+
+
+@Component({
+  selector: 'dialog-add-category',
+  templateUrl: '../templates/dialog-add-category.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule , FormsModule, ReactiveFormsModule ],
+})
+export class DialogAddCategory implements OnInit { 
+  protected newInformationForm!: FormGroup;
+  
+  constructor(
+    private readonly formBuilder: NonNullableFormBuilder,
+  ){}
+
+  get newUserFormControl() {
+    return this.newInformationForm.controls;
+  }
+
+  onSubmit() {
+    //this.userService.addUser(this.newUserForm.value as User)
+    //  .subscribe( user => {
+        return this.newInformationForm.value as Category;
+    //  })
+  }
+
+  ngOnInit(): void {
+    this.newInformationForm = this.formBuilder.group({
+      title: new FormControl('', [
+        Validators.required,
+      ]),
+      content: new FormControl('', [
+        Validators.required,
+      ]),
+      link: new FormControl('', [
+        Validators.pattern('[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+      ]),
+      remindDate: new FormControl('', [
+      ]),
+      category: new FormControl('', [
+        Validators.required
+      ]),
+        }
+    )
+  }
 }
